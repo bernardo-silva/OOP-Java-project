6bfd98a47d1a2777355005fa089af6214e94e922
vsil <vicente.cruz.sil@gmail.com>
2022-06-09 13:33:22 +0100

    started work on Player

--- src/videopoker/Player.java
+++ src/videopoker/Player.java
@@ -1,5 +1,30 @@
 package videopoker;
 
 public class Player {
+	private int credits;
+	private int money;
+	private int last_bet;
+	private String strategy;
+	
+	public Player(int _money, String _strategy, String _hand) {
+		money = _money;
+		strategy = _strategy;		
+	}
+		
+	public void bet(int i) {
+	// A b command typed after the
+	//	deal and before the end of the dealer’s turn is illegal, and so it should be printed in the terminal
+	//	‘‘b: illegal command’’.
+		money = money-i;
+		last_bet = i;
+	}
 
+	public void bet() {
+		if(last_bet==0) {
+			money = money - 5;
+			last_bet = 5;
+		}
+		money = money - last_bet;		
+	}
+		
 }
